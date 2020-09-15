b79c978ee6e47453b033731a837948e50372335b
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2020-09-15 13:24:48 -0300

    Aula 23 Projeto de Sistema
    Aula 23 Projeto de Sistema

--- src/Aula22ex107/principal.py
+++ src/Aula22ex107/principal.py
@@ -7,9 +7,7 @@ a = int(input('Aumento %? '))
 d = int(input('Desconto %? '))
 moeda.resumo(n, a, d)
 
-
 '''print(f'Metade de {moeda.moeda(n)} = {moeda.metade(n, True)}')
 print(f'Dobro de {moeda.moeda(n)} = {moeda.dobro(n, True)}')
 print(f'Aumento de {p} % = {(moeda.aumentar(n, p, True))}')
 print(f'Desconto de {p} % = {(moeda.diminuir(n, p, False))}')'''
-
---
+++ src/Aula23ex115Sistema/Sistema.py
@@ -0,0 +1,27 @@
+# PROGRAMA PRINCIPAL DE SISTEMA
+#
+from lib.interface import *
+from lib.arquivo import *
+from time import sleep
+
+arq = 'arqsistema.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver Cadastrados','Cadastrar Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        #Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabecalho('Saindo do Sistema')
+        break
+    else:
+        print('\033[31mERRO: Escolha opção novamente!\033[m')
+    sleep(2)
